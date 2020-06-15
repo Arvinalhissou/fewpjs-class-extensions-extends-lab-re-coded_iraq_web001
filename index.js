66f8e59277a6de8533e35812e7fec42d45df526f
Arvin  Alhissou <arvin.alhissou@gmail.com>
2020-06-15 10:40:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Your code here
+class Polygon {
+  constructor(array) {
+    this.sides = array
+  }
+
+  get getCount() {
+    return this.sides.length
+  }
+
+  get perimeter() {
+    return this.sides.reduce((sum, currentSide) => sum + currentSide, 0)
+  }
+}
